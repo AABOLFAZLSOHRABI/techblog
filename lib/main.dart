185aab9557214d7c99d0984f465cd2ce9aa36999
AABOLFAZLSOHRABI <abolfazl2277999@gmail.com>
2025-02-06 15:28:28 +0330

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:techblog/my_colors.dart';
 import 'splash_screen.dart';
 import 'main_screen.dart';
 // import 'gen/assets.gen.dart';
 
 void main() {
+  void main() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: SolidColors.statusBarColor,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: SolidColors.statusBarColor,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+  }
+
   runApp(const MyApp());
 }
 
@@ -31,7 +43,8 @@ class MyApp extends StatelessWidget {
           displayLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
           displayMedium: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
           displaySmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          headlineMedium: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+          headlineMedium:
+              TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
           titleLarge: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
           bodyLarge: TextStyle(fontSize: 16.0),
           bodyMedium: TextStyle(fontSize: 14.0),
